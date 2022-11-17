618c9576df26d6ac41abf1016fb0ff9cf850e77d
MyNameIsHeart <dovahkiin@DESKTOP-MF1PGO8>
2022-11-17 17:20:18 +0200

    added images to portfolio

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,14 +2,14 @@ export default[
     // each object is a project
     {
         title: 'Pseudo Assembler',
-        imgUrl: '', // to be added later
+        imgUrl: '/assets/Pseudo_Assembler_Image.png',
         imtAlt: 'Pseudo_Assembler_Image',
         stack: ['Pseudo-Assembly', 'C'], // related technologies
         link: 'https://github.com/MyNameIsHeart/pseudo_assembler',
     },
     {
         title: 'Minimal Portfolio',
-        imgUrl: '', // to be added later
+        imgUrl: '/assets/Minimal_Portfolio_Image.png',
         imtAlt: 'Minimal_Portfolio_Image',
         stack: ['React', 'Tailwind', 'Vite'], // related technologies
         link: '', // add github link when finished
