c5b0b4185bb73b0fdc8a128b500084ceee08bb11
MyNameIsHeart <dovahkiin@DESKTOP-MF1PGO8>
2022-11-17 16:31:19 +0200

    worked on the intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 
+/*
+    tailwindcss doc (padding):
+    https://tailwindcss.com/docs/padding 
+*/
+
 function Intro() {
     return (
-        <div></div>
+        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6"> {/* intro styling */}
+            
+            <h1
+                className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold"
+            >
+                Noam L.
+            </h1> {/* name */}
+
+            <p
+                className="text-base md:text-xl mb-3 font-medium"
+            >
+                Software Developer Intern & Computer Science Student
+            </p> {/* job title */}
+            
+            <p
+                className="text-sm max-w-xl mb-6 font-bold"
+            >
+                Self learner, persistent and accurate, “can do” approach, a team player.<br />
+                Looking for a role where I can work as part of a team towards a meaningful goal.<br />
+                I'm Always in pursuit of meaning, intriguing challenges, and interesting things to learn.
+            </p> {/* bio */}
+
+        </div>
     )
 }
 
