68c9b2e342fd923de1964470bf2335c55938964c
MyNameIsHeart <dovahkiin@DESKTOP-MF1PGO8>
2023-11-25 18:18:42 +0200

    updated the intro component (data and style)

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,7 +7,7 @@ import React from "react";
 
 function Intro() {
     return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6"> {/* intro styling */}
+        <div className="flex items-center text-center justify-center flex-col pt-20 pb-6"> {/* intro styling */}
             
             <h1
                 className="text-4xl md:text-7xl lg:text-8xl dark:text-white mb-1 md:mb-3 lg:mb-6 font-bold"
@@ -21,15 +21,23 @@ function Intro() {
                 Software Developer at Check Point
             </p> {/* job title */}
             
-            <p
-                className="text-sm lg:text-lg max-w-4xl mb-6 lg:mb-8 font-bold"
-            >
-                I’m a software developer (intern) at Check Point with an interest in cyber security.<br />
-                I am looking for my next entry role where I can expand my knowledge and contend with more<br />complex research and development challenges.
 
-            </p> {/* bio */}
+            {/* bio */}
+
+            <p
+                className="text-left text-sm lg:text-lg max-w-4xl mb-6 lg:mb-8 font-bold"
+            >
+                I am looking for my next entry role where I can expand my knowledge and contend with more
+                <br/>
+                complex research and development challenges.
+                <br/><br/>
+                I am a team player and a quick learner with experience programming in C and Python
+                <br/>
+                as well as using other technologies and platforms such as Linux, Git, VMs and Jira.
+            </p>
 
         </div>
+        
     )
 }
 
